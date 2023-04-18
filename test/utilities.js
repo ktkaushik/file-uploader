deb798ca7ef1c7b32afa3324ef8f053079dcafb1
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2023-04-18 15:48:14 +1000

    adding mocha and test cases for utilities

---
+++ test/utilities.js
@@ -0,0 +1,51 @@
+const assert = require('assert')
+const {
+    getPrivateKey,
+    base64Encode,
+    base64Decode,
+    getEncodedFolderNameForIP,
+    getFolderNameFromPrivateKey
+} = require('../lib/utilities')
+
+const ip = '127.0.0.1'
+
+describe('Test utilities', function () {
+
+    describe('getPrivateKey', function () {
+        it('should generate a private key from an IP address', function () {
+            const privateKey = getPrivateKey(ip)
+            assert.equal(privateKey, 'TVRJM0xqQXVNQzR4')
+        })
+    })
+
+    describe('base64Encode', function () {
+        it('should encode a string as base64', function () {
+            const encoded = base64Encode('Hello World')
+            assert.equal(encoded, 'SGVsbG8gV29ybGQ=')
+        })
+    })
+
+    describe('base64Decode', function () {
+        it('should decode a base64 string back to its original value', function () {
+            const decoded = base64Decode('SGVsbG8gV29ybGQ=')
+            assert.equal(decoded, 'Hello World')
+        })
+    })
+
+    describe('getEncodedFolderNameForIP', function () {
+        it('should generate an encoded folder name from a public and private key', function () {
+            const publicKey = base64Encode(ip)
+            const privateKey = getPrivateKey(ip)
+            console.log(publicKey, privateKey)
+            const folderName = getEncodedFolderNameForIP(publicKey, privateKey)
+            assert.equal(folderName, '127.0.0.1--MTI3LjAuMC4x--TVRJM0xqQXVNQzR4')
+        })
+    })
+
+    describe('getFolderNameFromPrivateKey', function () {
+        it('should generate a folder name from a private key', function () {
+            const folderName = getFolderNameFromPrivateKey('TVRJM0xqQXVNQzR4')
+            assert.equal(folderName, '127.0.0.1--MTI3LjAuMC4x--TVRJM0xqQXVNQzR4')
+        })
+    })
+})
